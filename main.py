c576a0b80322e6a389374a739f21a4cd1c310bc3
Rafe12345 <rafechan27@gmail.com>
2024-11-20 20:08:15 +0800

    feat: add obstacles

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import pygame, sys
 from player import Player
+import obstacle
 class Game:
     def __init__(self):
+        #player setup
         player_sprite = Player((screen_width/2,screen_height-20))
         self.player = pygame.sprite.GroupSingle(player_sprite)
+
+        #obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 5
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(40,600,*self.obstacle_x_positions)
+
+    def create_obstacle(self, x_start, y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for column_index, column in enumerate(row):
+                if column == 'x':
+                    x = x_start + column_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size,(220,20,60),x,y)
+                    self.blocks.add(block)
+    def create_multiple_obstacles(self,x_start,y_start,*offset):
+        for x in offset:
+            self.create_obstacle(x_start,y_start,x)
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+        self.blocks.draw(screen)
 
 pygame.init()
 screen_width = 800  
@@ -20,7 +43,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill((10,10,10))
+    screen.fill((10,10,10)) 
     game.run()
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(60) #Sets the fps to be 60
